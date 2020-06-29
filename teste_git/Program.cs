6b0762f3fb6058475977ba25b5161a0335042da8
AlexsandroFerreira <ferreira.alex@outlook.com>
2020-06-29 09:37:21 -0300

    projeto iniciado, teste ola mundo

---
+++ teste_git/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace teste_git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
