01e7646fafdedded79e02d9df35ad016f02a8eb5
AlexsandroFerreira <ferreira.alex@outlook.com>
2020-06-29 10:27:44 -0300

    teste do 3 commit

--- teste_git/Program.cs
+++ teste_git/Program.cs
@@ -4,8 +4,10 @@ namespace teste_git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
             Console.WriteLine("segundo commit");
+            Console.WriteLine("terceiro commit");
+            Console.ReadLine();
+
         }
     }
 }
